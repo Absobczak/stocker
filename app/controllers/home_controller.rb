e8ab808e4ab2977b6e97175c517fe07ee43cb4e7
Andrew Sobczak <andrew.sobczak@gmail.com>
2018-09-12 07:51:01 +0000

    Added Stock Quote and Devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    if params[:id] == ""
+      @nothing = "Hey, you forgot to enter a symbol"
+    elsif
+      if params[:id]
+          @stock = StockQuote::Stock.quote(params[:id])
+      end
+    elsif params[:id] != params[:id]
+      @error = "That Stock Symbol Doesn't Exist...Please Try Again."
+    end
   end
   
   def about
